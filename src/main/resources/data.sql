f1414002be487148c9391c7f6bd7f114294b7d10
Renato Ibanhez <renato.ibanhez@walmart.com>
2017-07-31 19:29:07 -0300

    [homolog][ci]: Configuring liquibase

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- Move it to Liquibase
+INSERT INTO partnership (id, name, reference) VALUES ('asd1', 'Comparador', 'COMPARADOR');
+INSERT INTO partnership (id, name, reference) VALUES ('asd2', 'AFILIADO', 'Afiliado');
+INSERT INTO partnership (id, name, reference) VALUES ('asd3', 'Search Engine Marketing', 'SEM');
+INSERT INTO partnership (id, name, reference) VALUES ('asd4', 'Email marketing', 'EMAIL_MARKETING');
+INSERT INTO partnership (id, name, reference) VALUES ('asd5', 'B2B', 'B2B');
+INSERT INTO partnership (id, name, reference) VALUES ('asd6', 'Retargeting', 'RETARGETING');
