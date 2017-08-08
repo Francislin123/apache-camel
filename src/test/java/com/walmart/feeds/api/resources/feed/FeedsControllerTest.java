0d6718cede681941d15403bf5a6b49c4e4c2fc95
Renato Ibanhez <renato.ibanhez@walmart.com>
2017-08-08 16:39:20 -0300

    [FDS-1286]: Solving bug on creating/updating feed when utm list is invalid.
    For example:
    "utms": {
    	"utm_source": null,
    	"utm_medium": "",
    	"utm_teste": "    "
    }

--- src/test/java/com/walmart/feeds/api/resources/feed/FeedsControllerTest.java
+++ src/test/java/com/walmart/feeds/api/resources/feed/FeedsControllerTest.java
@@ -96,6 +96,30 @@ public class FeedsControllerTest {
     }
 
     @Test
+    public void testCreateFeedWithInvalidUtmList() throws Exception {
+
+        when(feedService.createFeed(any(FeedEntity.class))).thenReturn(new FeedEntity());
+
+        mockMvc.perform(
+                post(FeedsController.V1_FEEDS, "partnerReferenceTest").contentType(MediaType.APPLICATION_JSON).content(asJsonString(Fixture.from(FeedRequest.class).gimme("feed-full-with-invalid-utm-list")))
+        ).andExpect(status().isBadRequest());
+
+        verify(feedService, times(0)).createFeed(any(FeedEntity.class));
+    }
+
+    @Test
+    public void testCreateFeedWithAnEmptyName() throws Exception {
+
+        when(feedService.createFeed(any(FeedEntity.class))).thenReturn(new FeedEntity());
+
+        mockMvc.perform(
+                post(FeedsController.V1_FEEDS, "partnerReferenceTest").contentType(MediaType.APPLICATION_JSON).content(asJsonString(Fixture.from(FeedRequest.class).gimme("feed-full-with-empty-name")))
+        ).andExpect(status().isBadRequest());
+
+        verify(feedService, times(0)).createFeed(any(FeedEntity.class));
+    }
+
+    @Test
     public void testCreateFeedWhenRequestIsIvalid() throws Exception {
 
         when(feedService.createFeed(any(FeedEntity.class))).thenReturn(new FeedEntity());
