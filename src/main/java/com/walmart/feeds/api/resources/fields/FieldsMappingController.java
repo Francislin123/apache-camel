e60a459b15ca1bf6ae978c841d928dc7fbffc864
Francislin dos Reis <vn0gshm@walmart.com>
2017-08-09 13:08:30 -0300

    [FDS-1025] [FDS-1189] Create new Fields Mapping ok

--- src/main/java/com/walmart/feeds/api/resources/fields/FieldsMappingController.java
+++ src/main/java/com/walmart/feeds/api/resources/fields/FieldsMappingController.java
@@ -2,23 +2,67 @@ package com.walmart.feeds.api.resources.fields;
 
 import com.walmart.feeds.api.core.repository.fields.model.FieldsMappingEntity;
 import com.walmart.feeds.api.core.repository.fields.model.MappedFieldEntity;
+import com.walmart.feeds.api.core.service.feed.FeedService;
+import com.walmart.feeds.api.core.service.feed.FeedServiceImpl;
 import com.walmart.feeds.api.core.service.fields.FieldsMappingService;
+import com.walmart.feeds.api.core.utils.SlugParserUtil;
 import com.walmart.feeds.api.resources.fields.request.FieldsMappingRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.util.stream.Collectors;
 
+/**
+ * Created by vn0gshm on 08/08/17.
+ */
+@Api
 @RestController
-@RequestMapping(FieldsMappingController.V1_FIELDS_MAPPING)
+@RequestMapping(FieldsMappingController.URI_FIELDSDMAPPING)
 public class FieldsMappingController {
 
-    public static final String V1_FIELDS_MAPPING = "/v1/fieldsmapping";
+    public static final String URI_FIELDSDMAPPING = "/v1/fieldsmapping";
+
+    private Logger logger = LoggerFactory.getLogger(FieldsMappingController.class);
 
     @Autowired
     private FieldsMappingService fieldsMappingService;
+
+    //@Autowired
+    private FeedService feedService = new FeedServiceImpl();
+
+    @ApiOperation(value = "Create new fields mapping",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = " Successful new fields mapping ", response = FieldsMappingRequest.class),
+            @ApiResponse(code = 409, message = " FieldsMappingEntity already exists "),
+            @ApiResponse(code = 500, message = " Unhandled exception ")})
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<?> createFieldsMapping(@RequestBody @Valid FieldsMappingRequest fieldsMappingRequest, UriComponentsBuilder builder) {
+
+        FieldsMappingEntity fieldsMapping;
+        fieldsMapping = FieldsMappingEntity.builder()
+                .mappedFields(fieldsMappingRequest.getMappedFields())
+                .name(fieldsMappingRequest.getName())
+                .slug(SlugParserUtil.toSlug(fieldsMappingRequest.getName()))
+                .build();
+
+        fieldsMappingService.saveFieldsdMapping(fieldsMapping);
+        UriComponents uriComponents = builder.path(URI_FIELDSDMAPPING.concat("/{partnerSlug}")).buildAndExpand(fieldsMapping.getSlug());
+
+        return ResponseEntity.created(uriComponents.toUri()).build();
+
+    }
 
     @RequestMapping(value = "{slug}", method = RequestMethod.PUT)
     public ResponseEntity updateMapping(@RequestBody @Valid FieldsMappingRequest request,
@@ -27,20 +71,20 @@ public class FieldsMappingController {
         // TODO: 09/08/17 validate null elements on request.mappedfields
 
         FieldsMappingEntity mappingEntity = FieldsMappingEntity.builder()
-            .name(request.getName())
-            .slug(slug)
-            .mappedFields(
-                request.getMappedFields().stream().map(m -> MappedFieldEntity.builder()
-                    .partnerField(m.getPartnerField())
-                    .wmField(m.getWmField())
-                    .required(m.isRequired())
-                    .build())
-                .collect(Collectors.toList()))
-            .build();
+                .name(request.getName())
+                .slug(slug)
+                .mappedFields(
+                        request.getMappedFields().stream().map(m -> MappedFieldEntity.builder()
+                                .partnerField(m.getPartnerField())
+                                .wmField(m.getWmField())
+                                .required(m.isRequired())
+                                .build())
+                                .collect(Collectors.toList()))
+                .build();
 
         fieldsMappingService.updateFieldsMapping(mappingEntity);
 
         return ResponseEntity.ok().build();
     }
-
 }
+
---
+++ src/test/java/com/walmart/feeds/api/unit/resources/fields/test/template/FieldsMappingTemplateLoader.java
@@ -0,0 +1,60 @@
+package com.walmart.feeds.api.unit.resources.fields.test.template;
+
+import br.com.six2six.fixturefactory.Fixture;
+import br.com.six2six.fixturefactory.Rule;
+import br.com.six2six.fixturefactory.loader.TemplateLoader;
+import com.walmart.feeds.api.core.repository.fields.model.FieldsMappingEntity;
+import com.walmart.feeds.api.core.repository.fields.model.MappedFieldEntity;
+import com.walmart.feeds.api.core.repository.partner.model.PartnerEntity;
+import com.walmart.feeds.api.resources.fields.request.FieldsMappingRequest;
+import com.walmart.feeds.api.resources.partner.request.PartnerRequest;
+import com.walmart.feeds.api.resources.partner.request.PartnerUpdateRequest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FieldsMappingTemplateLoader implements TemplateLoader {
+
+    @Override
+    public void load() {
+        Fixture.of(MappedFieldEntity.class).addTemplate("mapped_field", new Rule() {{
+            add("wmField", "Buscape");
+            add("partnerField", "descricao");
+            add("required", true);
+        }});
+
+        List<MappedFieldEntity> mappedFields = new ArrayList<>();
+        mappedFields.add(Fixture.from(MappedFieldEntity.class).gimme("mapped_field"));
+        Fixture.of(FieldsMappingRequest.class).addTemplate("valid_fields_mapping_request", new Rule() {{
+            add("name", "Buscape");
+            add("mappedFields", mappedFields);
+        }});
+
+        List<MappedFieldEntity> mappedFields01 = new ArrayList<>();
+        mappedFields01.add(Fixture.from(MappedFieldEntity.class).gimme("mapped_field"));
+        Fixture.of(FieldsMappingRequest.class).addTemplate("valid_fields_mapping_notblank", new Rule() {{
+            add("name", "");
+            add("mappedFields", mappedFields);
+        }});
+
+        Fixture.of(FieldsMappingRequest.class).addTemplate("invalid_fields_mapping_request_no_mapped_fields", new Rule() {{
+            add("name", "Buscape");
+        }});
+
+        Fixture.of(PartnerUpdateRequest.class).addTemplate("fields_mapping_update_request", new Rule() {{
+            add("name", "Buscape");
+            add("mappedFields", Arrays.asList("price"));
+        }});
+
+        Fixture.of(PartnerUpdateRequest.class).addTemplate("invalid_fields_mapping_udpate_request", new Rule() {{
+            add("name", "Buscape");
+            add("mappedFields", Arrays.asList("price"));
+        }});
+
+        Fixture.of(PartnerEntity.class).addTemplate("fields_mapping_entity", new Rule() {{
+            add("name", "Buscape");
+            add("mappedFields", Arrays.asList("price"));
+        }});
+    }
+}
