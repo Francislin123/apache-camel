1b04ba237f76a3236afff71bc0398f8d66a6da6b
Renato Ibanhez <renato.ibanhez@walmart.com>
2017-08-09 15:25:43 -0300

    [FDS-1288]: Solving bug on create/update partners with invalid partnerships

---
+++ src/main/java/com/walmart/feeds/api/resources/feed/validator/annotation/NotEmptyElements.java
@@ -0,0 +1,24 @@
+package com.walmart.feeds.api.resources.feed.validator.annotation;
+
+import com.walmart.feeds.api.resources.feed.validator.NotEmptyElementsValidator;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = NotEmptyElementsValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@NotEmpty
+public @interface NotEmptyElements {
+
+    String message() default "{com.walmart.validation.constraints.NotEmptyElements.message}";
+
+    String allowedPattern() default "*";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
