edc8e727476fa06890c0487011cccec8655152fe
Thiago Lima <thiago.j.lima@walmart.com>
2017-08-23 13:59:53 -0300

    [FDS-1033][FIX-ON-MERGE] Fixing general code.

--- src/test/java/com/walmart/feeds/api/unit/resources/taxonomy/PartnerTaxonomyServiceTest.java
+++ src/test/java/com/walmart/feeds/api/unit/resources/taxonomy/PartnerTaxonomyServiceTest.java
@@ -2,26 +2,39 @@ package com.walmart.feeds.api.unit.resources.taxonomy;
 
 import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
+import com.walmart.feeds.api.core.exceptions.EntityAlreadyExistsException;
 import com.walmart.feeds.api.core.exceptions.EntityNotFoundException;
 import com.walmart.feeds.api.core.repository.taxonomy.PartnerTaxonomyHistoryRepository;
 import com.walmart.feeds.api.core.repository.taxonomy.PartnerTaxonomyRepository;
+import com.walmart.feeds.api.core.repository.taxonomy.model.ImportStatus;
 import com.walmart.feeds.api.core.repository.taxonomy.model.PartnerTaxonomyEntity;
 import com.walmart.feeds.api.core.repository.taxonomy.model.PartnerTaxonomyHistory;
 import com.walmart.feeds.api.core.repository.partner.model.PartnerEntity;
 import com.walmart.feeds.api.core.service.taxonomy.PartnerTaxonomyService;
 import com.walmart.feeds.api.core.service.taxonomy.PartnerTaxonomyServiceImpl;
 import com.walmart.feeds.api.core.service.partner.PartnerService;
+import com.walmart.feeds.api.resources.camel.PartnerTaxonomyRouteBuilder;
+import com.walmart.feeds.api.resources.camel.TaxonomyMappingBindy;
+import com.walmart.feeds.api.resources.taxonomy.request.UploadTaxonomyMappingTO;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.mock.web.MockMultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static com.walmart.feeds.api.resources.camel.PartnerTaxonomyRouteBuilder.VALIDATE_FILE_ROUTE;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -40,18 +53,125 @@ public class PartnerTaxonomyServiceTest {
     @Mock
     private PartnerService partnerService;
 
+    @Mock
+    private ProducerTemplate producerTemplate;
+
     @BeforeClass
     public static void init(){
         FixtureFactoryLoader.loadTemplates("com.walmart.feeds.api.unit.resources.taxonomy.test.template");
     }
+
+    @Test(expected = EntityAlreadyExistsException.class)
+    public void testWhenPartnerTaxonomyIsPending() throws IOException {
+
+        UploadTaxonomyMappingTO to = Fixture.from(UploadTaxonomyMappingTO.class).gimme("to-mapping");
+
+        PartnerTaxonomyEntity pendingPartnerEntity = PartnerTaxonomyEntity.builder()
+                .status(ImportStatus.PENDING)
+            .build();
+
+        when(partnerService.findBySlug(to.getPartnerSlug())).thenReturn(Fixture.from(PartnerEntity.class).gimme("partner_entity"));
+        when(partnerTaxonomyRepository.findBySlug(to.getSlug())).thenReturn(Optional.of(pendingPartnerEntity));
+
+        partnerTaxonomyService.processFile(to);
+
+    }
+
+    @Test(expected = EntityAlreadyExistsException.class)
+    public void testWhenPartnerTaxonomyIsInitial() throws IOException {
+
+        UploadTaxonomyMappingTO to = Fixture.from(UploadTaxonomyMappingTO.class).gimme("to-mapping");
+
+        PartnerTaxonomyEntity pendingPartnerEntity = PartnerTaxonomyEntity.builder()
+                .status(ImportStatus.INITIAL)
+                .build();
+
+        when(partnerService.findBySlug(to.getPartnerSlug())).thenReturn(Fixture.from(PartnerEntity.class).gimme("partner_entity"));
+        when(partnerTaxonomyRepository.findBySlug(to.getSlug())).thenReturn(Optional.of(pendingPartnerEntity));
+
+        partnerTaxonomyService.processFile(to);
+
+    }
+
     @Test
-    public void testDeleteBeforeUpdate(){
-        PartnerTaxonomyEntity partnerTaxonomyEntity = Fixture.from(PartnerTaxonomyEntity.class).gimme("cs-input-ok");
-        when(partnerTaxonomyRepository.findBySlug(partnerTaxonomyEntity.getSlug())).thenReturn(Optional.of(partnerTaxonomyEntity));
-        when(partnerService.findBySlug(partnerTaxonomyEntity.getPartner().getSlug())).thenReturn(new PartnerEntity());
-        when(partnerTaxonomyRepository.saveAndFlush(partnerTaxonomyEntity)).thenReturn(partnerTaxonomyEntity);
-//        this.partnerTaxonomyService.loadFile(partnerTaxonomyEntity);
-        verify(partnerTaxonomyRepository, times(1)).delete(partnerTaxonomyEntity);
+    public void testWhenPartnerTaxonomyIsProcessed() throws IOException {
+
+        UploadTaxonomyMappingTO to = Fixture.from(UploadTaxonomyMappingTO.class).gimme("to-mapping");
+
+        PartnerTaxonomyEntity pendingPartnerEntity = PartnerTaxonomyEntity.builder()
+                .status(ImportStatus.PROCESSED)
+                .build();
+
+        List<TaxonomyMappingBindy> bindyList = new ArrayList<>();
+        bindyList.add(Fixture.from(TaxonomyMappingBindy.class).gimme("taxonomy-bindy"));
+
+        PartnerTaxonomyHistory history = Fixture.from(PartnerTaxonomyHistory.class).gimme("cs-history-input-ok");
+
+        when(partnerService.findBySlug(to.getPartnerSlug())).thenReturn(Fixture.from(PartnerEntity.class).gimme("partner_entity"));
+        when(partnerTaxonomyRepository.findBySlug(to.getSlug())).thenReturn(Optional.of(pendingPartnerEntity));
+        when(producerTemplate.requestBody(VALIDATE_FILE_ROUTE, to.getTaxonomyMapping().getInputStream(), List.class)).thenReturn(bindyList);
+
+        when(partnerTaxonomyRepository.saveAndFlush(any(PartnerTaxonomyEntity.class))).thenReturn(pendingPartnerEntity);
+        when(partnerTaxonomyHistoryRepository.saveAndFlush(history)).thenReturn(history);
+
+        partnerTaxonomyService.processFile(to);
+
+        verify(partnerTaxonomyRepository, times(1)).delete(eq(pendingPartnerEntity));
+        verify(partnerTaxonomyRepository, times(1)).flush();
+
+    }
+
+    @Test
+    public void testWhenPartnerTaxonomyIsError() throws IOException {
+
+        UploadTaxonomyMappingTO to = Fixture.from(UploadTaxonomyMappingTO.class).gimme("to-mapping");
+
+        PartnerTaxonomyEntity pendingPartnerEntity = PartnerTaxonomyEntity.builder()
+                .status(ImportStatus.ERROR)
+                .build();
+
+        List<TaxonomyMappingBindy> bindyList = new ArrayList<>();
+        bindyList.add(Fixture.from(TaxonomyMappingBindy.class).gimme("taxonomy-bindy"));
+
+        PartnerTaxonomyHistory history = Fixture.from(PartnerTaxonomyHistory.class).gimme("cs-history-input-ok");
+
+        when(partnerService.findBySlug(to.getPartnerSlug())).thenReturn(Fixture.from(PartnerEntity.class).gimme("partner_entity"));
+        when(partnerTaxonomyRepository.findBySlug(to.getSlug())).thenReturn(Optional.of(pendingPartnerEntity));
+        when(producerTemplate.requestBody(VALIDATE_FILE_ROUTE, to.getTaxonomyMapping().getInputStream(), List.class)).thenReturn(bindyList);
+
+        when(partnerTaxonomyRepository.saveAndFlush(any(PartnerTaxonomyEntity.class))).thenReturn(pendingPartnerEntity);
+        when(partnerTaxonomyHistoryRepository.saveAndFlush(history)).thenReturn(history);
+
+        partnerTaxonomyService.processFile(to);
+
+        verify(partnerTaxonomyRepository, times(1)).delete(eq(pendingPartnerEntity));
+        verify(partnerTaxonomyRepository, times(1)).flush();
+
+    }
+
+    @Test
+    public void testWhenPartnerTaxonomyNotExists() throws IOException {
+
+        UploadTaxonomyMappingTO to = Fixture.from(UploadTaxonomyMappingTO.class).gimme("to-mapping");
+
+        List<TaxonomyMappingBindy> bindyList = new ArrayList<>();
+        bindyList.add(Fixture.from(TaxonomyMappingBindy.class).gimme("taxonomy-bindy"));
+
+        PartnerTaxonomyEntity partnerTaxonomyEntity = mock(PartnerTaxonomyEntity.class);
+
+        PartnerTaxonomyHistory history = Fixture.from(PartnerTaxonomyHistory.class).gimme("cs-history-input-ok");
+
+        when(partnerService.findBySlug(to.getPartnerSlug())).thenReturn(Fixture.from(PartnerEntity.class).gimme("partner_entity"));
+        when(partnerTaxonomyRepository.findBySlug(to.getSlug())).thenReturn(Optional.empty());
+        when(producerTemplate.requestBody(VALIDATE_FILE_ROUTE, to.getTaxonomyMapping().getInputStream(), List.class)).thenReturn(bindyList);
+        when(partnerTaxonomyRepository.saveAndFlush(any(PartnerTaxonomyEntity.class))).thenReturn(partnerTaxonomyEntity);
+
+        when(partnerTaxonomyHistoryRepository.saveAndFlush(history)).thenReturn(history);
+
+        partnerTaxonomyService.processFile(to);
+
+        verify(partnerTaxonomyRepository, never()).delete(eq(partnerTaxonomyEntity));
+        verify(partnerTaxonomyRepository, never()).flush();
 
     }
 
@@ -67,12 +187,16 @@ public class PartnerTaxonomyServiceTest {
     }
     @Test
     public void deleteMappedTaxonomy(){
+
         PartnerTaxonomyEntity partnerTaxonomyEntity = Fixture.from(PartnerTaxonomyEntity.class).gimme("cs-input-ok");
-        when(partnerTaxonomyRepository.findBySlug("existentSlug")).thenReturn(Optional.of(partnerTaxonomyEntity));
-        when(partnerService.findBySlug("validPartner")).thenReturn(new PartnerEntity());
+        when(partnerService.findBySlug(partnerTaxonomyEntity.getPartner().getSlug())).thenReturn(partnerTaxonomyEntity.getPartner());
+        when(partnerTaxonomyRepository.findBySlugAndPartner(partnerTaxonomyEntity.getSlug(), partnerTaxonomyEntity.getPartner()))
+                .thenReturn(Optional.of(partnerTaxonomyEntity));
         doNothing().when(partnerTaxonomyRepository).delete(partnerTaxonomyEntity);
-        this.partnerTaxonomyService.removeEntityBySlug("validPartner", "existentSlug");
-        verify(partnerTaxonomyRepository, times(1)).delete(partnerTaxonomyEntity);
+
+        this.partnerTaxonomyService.removeEntityBySlug(partnerTaxonomyEntity.getPartner().getSlug(), partnerTaxonomyEntity.getSlug());
+
+        verify(partnerTaxonomyRepository, times(1)).delete(eq(partnerTaxonomyEntity));
 
     }
     @Test(expected = EntityNotFoundException.class)
@@ -95,14 +219,10 @@ public class PartnerTaxonomyServiceTest {
     @Test
     public void fetchTaxonomyBySlugAndPartner(){
         PartnerTaxonomyEntity partnerTaxonomyEntity = Fixture.from(PartnerTaxonomyEntity.class).gimme("cs-input-ok");
-        List<PartnerTaxonomyEntity> list = new ArrayList<>();
-        list.add(partnerTaxonomyEntity);
-
-        when(partnerTaxonomyRepository.findBySlug(partnerTaxonomyEntity.getSlug())).thenReturn(Optional.of(partnerTaxonomyEntity));
+        PartnerTaxonomyEntity serviceResponse = this.partnerTaxonomyService.fetchProcessedPartnerTaxonomy(partnerTaxonomyEntity.getPartner().getSlug(),partnerTaxonomyEntity.getSlug());
         when(partnerService.findBySlug(partnerTaxonomyEntity.getPartner().getSlug())).thenReturn(partnerTaxonomyEntity.getPartner());
-        this.partnerTaxonomyService.fetchPartnerTaxonomies(partnerTaxonomyEntity.getPartner().getSlug(), partnerTaxonomyEntity.getSlug());
-        verify(partnerTaxonomyRepository, times(1)).findBySlug(partnerTaxonomyEntity.getSlug());
-
+        when(partnerTaxonomyRepository.findBySlugAndPartnerAndStatus(partnerTaxonomyEntity.getSlug(),partnerTaxonomyEntity.getPartner(),ImportStatus.PROCESSED)).thenReturn(Optional.of(partnerTaxonomyEntity));
+        assertEquals(partnerTaxonomyEntity,serviceResponse);
     }
     @Test
     public void fetchTaxonomyByPartner(){
--- src/test/java/com/walmart/feeds/api/unit/resources/taxonomy/test/template/PartnerTaxonomyTemplateLoader.java
+++ src/test/java/com/walmart/feeds/api/unit/resources/taxonomy/test/template/PartnerTaxonomyTemplateLoader.java
@@ -4,9 +4,12 @@ import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.Rule;
 import br.com.six2six.fixturefactory.loader.TemplateLoader;
 import com.walmart.feeds.api.core.repository.taxonomy.model.PartnerTaxonomyEntity;
+import com.walmart.feeds.api.core.repository.taxonomy.model.PartnerTaxonomyHistory;
 import com.walmart.feeds.api.core.repository.taxonomy.model.TaxonomyMappingEntity;
 import com.walmart.feeds.api.core.repository.partner.model.PartnerEntity;
+import com.walmart.feeds.api.resources.camel.TaxonomyMappingBindy;
 import com.walmart.feeds.api.resources.taxonomy.request.UploadTaxonomyMappingTO;
+import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.FileOutputStream;
 import java.util.ArrayList;
@@ -32,14 +35,35 @@ public class PartnerTaxonomyTemplateLoader implements TemplateLoader {
             add("partnerPath", "any string");
             add("walmartPath", "any string");
         }});
-
+        MockMultipartFile taxonomyMappingFile = new MockMultipartFile("file", "id ".getBytes());
         Fixture.of(UploadTaxonomyMappingTO.class).addTemplate("to-mapping", new Rule(){{
-            add("name", "Buscape");
-            add("slug", "buscape");
+            add("name", "any-name");
+            add("slug", "any-slug");
             add("partnerSlug", "buscape");
-        }});
+            add("taxonomyMapping", taxonomyMappingFile);
 
+        }});
+        Fixture.of(TaxonomyMappingBindy.class).addTemplate("taxonomy-bindy", new Rule(){{
+            add("structurePartnerId", "12345");
+            add("partnerTaxonomy", "loja > produtos");
+            add("walmartTaxonomy", "loja > produtos");
+
+        }});
         Fixture.of(PartnerTaxonomyEntity.class).addTemplate("cs-input-ok", new Rule(){{
+            add("name", "any-name");
+            add("fileName", "taxonomy-file.csv");
+            add("slug", "any-slug");
+            PartnerEntity partner = Fixture.from(PartnerEntity.class).gimme("partner_entity");
+            add("partner", partner);
+
+            TaxonomyMappingEntity csa = Fixture.from(TaxonomyMappingEntity.class)
+                    .gimme("csa-entity");
+            List<TaxonomyMappingEntity> csaList = new ArrayList<>();
+            csaList.add(csa);
+            add("taxonomyMappings", csaList);
+
+        }});
+        Fixture.of(PartnerTaxonomyHistory.class).addTemplate("cs-history-input-ok", new Rule(){{
             add("name", "any-name");
             add("fileName", "taxonomy-file.csv");
             add("slug", "any-slug");
