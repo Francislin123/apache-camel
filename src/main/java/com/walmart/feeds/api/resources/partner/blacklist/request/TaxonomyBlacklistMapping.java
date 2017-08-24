2acce271a31da6fcee4b183a9933d6898eda8cd1
Thiago Lima <thiago.j.lima@walmart.com>
2017-08-24 17:17:16 -0300

    feat(feed-admin-api): Create Taxonomy blacklists controller
    Creation of taxonomy blacklist controllers to insert/update/fetch/delete
    
    Fixes FDS-1462

---
+++ src/main/java/com/walmart/feeds/api/resources/partner/blacklist/request/TaxonomyBlacklistMapping.java
@@ -0,0 +1,11 @@
+package com.walmart.feeds.api.resources.partner.blacklist.request;
+
+/**
+ * Created by vn0y942 on 24/08/17.
+ */
+public class TaxonomyBlacklistMapping {
+
+    private String taxonomy;
+
+    private TaxonomyOwner owner;
+}
---
+++ src/main/java/com/walmart/feeds/api/resources/partner/blacklist/request/TaxonomyOwner.java
@@ -0,0 +1,7 @@
+package com.walmart.feeds.api.resources.partner.blacklist.request;
+
+/**
+ * Created by vn0y942 on 24/08/17.
+ */
+public enum TaxonomyOwner {
+}
