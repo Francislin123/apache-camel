ba9c342a0a0c0b15283693ad0e47e9938a25be56
Adisson Gomes <vn03fh2@walmart.com>
2017-08-25 09:49:02 -0300

    feat(FDS-1302): initial JPA taxonomy blacklist entities

---
+++ src/main/java/com/walmart/feeds/api/core/repository/blacklist/TaxonomyBlacklistMapping.java
@@ -0,0 +1,45 @@
+package com.walmart.feeds.api.core.repository.blacklist;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.Tolerate;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+/**
+ * Created by vn0y942 on 24/08/17.
+ */
+@Entity
+@Table(name = "taxonomy_blacklist_mapping")
+@Builder
+@Getter
+@JsonIgnoreProperties({"id"})
+public class TaxonomyBlacklistMapping {
+
+    @Id
+    @GeneratedValue(generator = "taxonomy_bkl_mapping_uuid_generator")
+    @GenericGenerator(name = "taxonomy_bkl_mapping_uuid_generator", strategy = "uuid2")
+    @ApiModelProperty(hidden = true)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String taxonomy;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TaxonomyOwner owner;
+
+    @Tolerate
+    public TaxonomyBlacklistMapping() {
+    }
+
+    public TaxonomyBlacklistMapping(UUID id, String taxonomy, TaxonomyOwner owner) {
+        this.id = id;
+        this.taxonomy = taxonomy;
+        this.owner = owner;
+    }
+}
--- src/main/java/com/walmart/feeds/api/resources/blacklist/request/TaxonomyBlacklistMapping.java
+++ src/main/java/com/walmart/feeds/api/resources/blacklist/request/TaxonomyBlacklistMapping.java
@@ -1,5 +1,7 @@
 package com.walmart.feeds.api.resources.blacklist.request;
 
+import com.walmart.feeds.api.core.repository.blacklist.TaxonomyOwner;
+
 /**
  * Created by vn0y942 on 24/08/17.
  */
