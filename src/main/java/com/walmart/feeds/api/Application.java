646905aae3e1b288770188527b2680acbaf9ef31
Francislin dos Reis <vn0gshm@walmart.com>
2017-08-28 09:51:46 -0300

    feat(FDS-1020): FDS-1458 - Insert collection attribute in feed,Validate collection in TagAdmin and Refresh Liquibase changelog

--- src/main/java/com/walmart/feeds/api/Application.java
+++ src/main/java/com/walmart/feeds/api/Application.java
@@ -1,8 +1,15 @@
 package com.walmart.feeds.api;
 
+import com.walmart.feeds.api.client.tags.TagAdmimCollectionClient;
+import feign.Feign;
+import feign.Logger;
+import feign.auth.BasicAuthRequestInterceptor;
+import feign.jackson.JacksonDecoder;
+import feign.okhttp.OkHttpClient;
+import feign.slf4j.Slf4jLogger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.LocaleResolver;
@@ -17,12 +24,20 @@ import java.util.Locale;
 @SpringBootApplication(scanBasePackages = "com.walmart.feeds.*")
 public class Application {
 
+    @Value("${tagadmin.username}")
+    private String tagAdminUser;
+
+    @Value("${tagadmin.password}")
+    private String tagAdminPassword;
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     /**
      * Create a locale resolver to
+     *
      * @return LocaleResolver
      */
     @Bean
@@ -32,4 +47,14 @@ public class Application {
         return resolver;
     }
 
+    @Bean
+    public TagAdmimCollectionClient tagAdmimCollectionClient() {
+        return Feign.builder()
+                .client(new OkHttpClient())
+                .decoder(new JacksonDecoder())
+                .logger(new Slf4jLogger(TagAdmimCollectionClient.class))
+                .logLevel(Logger.Level.FULL)
+                .requestInterceptor(new BasicAuthRequestInterceptor(tagAdminUser, tagAdminPassword))
+                .target(TagAdmimCollectionClient.class, "http://vip-cat-serv.qa.vmcommerce.intra/ws/");
+    }
 }
--- src/main/java/com/walmart/feeds/api/client/tags/TagAdminCollection.java
+++ src/main/java/com/walmart/feeds/api/client/tags/TagAdminCollection.java
@@ -2,6 +2,9 @@ package com.walmart.feeds.api.client.tags;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+
+import java.util.Date;
+
 /**
  * Created by vn0gshm on 24/08/17.
  */
@@ -14,5 +17,5 @@ public class TagAdminCollection
     private String name;
     private String domain;
     private String status;
-    private Integer expiresAt;
+    private Date expiresAt;
 }
