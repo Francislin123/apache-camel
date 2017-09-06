6b7f706a69cee05446133d2767c3b8964d976063
r0i001q <renato.ibanhez@walmart.com>
2017-09-06 18:11:24 -0300

    [homolog] Resolving merge problems.

--- src/main/java/com/walmart/feeds/api/core/exceptions/EntityInUseException.java
+++ src/main/java/com/walmart/feeds/api/core/exceptions/EntityInUseException.java
@@ -1,5 +1,7 @@
 package com.walmart.feeds.api.core.exceptions;
 
+import com.walmart.feeds.api.resources.common.response.SimpleError;
+
 import java.util.List;
 
 public class EntityInUseException extends UserException {
@@ -8,7 +10,7 @@ public class EntityInUseException extends UserException {
         super(message);
     }
 
-    public EntityInUseException(String message, List<String> exceptionList) {
+    public EntityInUseException(String message, List<SimpleError> exceptionList) {
         super(message, exceptionList);
     }
 
--- src/main/java/com/walmart/feeds/api/core/service/blacklist/taxonomy/TaxonomyBlacklistServiceImpl.java
+++ src/main/java/com/walmart/feeds/api/core/service/blacklist/taxonomy/TaxonomyBlacklistServiceImpl.java
@@ -10,12 +10,12 @@ import com.walmart.feeds.api.core.repository.blacklist.TaxonomyBlacklistHistoryR
 import com.walmart.feeds.api.core.repository.blacklist.TaxonomyBlacklistRepository;
 import com.walmart.feeds.api.core.repository.blacklist.model.TaxonomyBlacklistEntity;
 import com.walmart.feeds.api.core.repository.blacklist.model.TaxonomyBlacklistHistory;
-import com.walmart.feeds.api.core.repository.blacklist.model.TaxonomyBlacklistMapping;
 import com.walmart.feeds.api.core.repository.blacklist.model.TaxonomyOwner;
 import com.walmart.feeds.api.core.repository.feed.FeedRepository;
 import com.walmart.feeds.api.core.repository.feed.model.FeedEntity;
 import com.walmart.feeds.api.core.utils.MapperUtil;
 import com.walmart.feeds.api.core.utils.SlugParserUtil;
+import com.walmart.feeds.api.resources.common.response.SimpleError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -119,7 +119,7 @@ public class TaxonomyBlacklistServiceImpl implements TaxonomyBlacklistService {
 
         List<FeedEntity> feeds = feedRepository.findByTaxonomyBlacklist(toDelete);
         if(!feeds.isEmpty()) {
-            List<String> feedsSlugs = feeds.stream().map(FeedEntity::getSlug).collect(Collectors.toList());
+            List<SimpleError> feedsSlugs = feeds.stream().map(f -> SimpleError.builder().message(f.getSlug()).build()).collect(Collectors.toList());
             throw new EntityInUseException(String.format("Taxonomy blacklist '%s' is being used by one or more feeds", slug), feedsSlugs);
         }
 
@@ -128,11 +128,13 @@ public class TaxonomyBlacklistServiceImpl implements TaxonomyBlacklistService {
     }
 
     private void validateBlacklist(TaxonomyBlacklistEntity taxonomyBlacklistEntity) {
-        List<String> expList = taxonomyBlacklistEntity.getList().stream()
+        List<SimpleError> expList = taxonomyBlacklistEntity.getList().stream()
                 .filter(mapping ->
                         mapping.getOwner() == TaxonomyOwner.WALMART && !elasticSearchService.validateWalmartTaxonomy(mapping.getTaxonomy())
                 )
-                .map(TaxonomyBlacklistMapping::getTaxonomy)
+                .map(m -> SimpleError.builder()
+                        .message(m.getTaxonomy())
+                        .build())
                 .collect(Collectors.toList());
         if(!expList.isEmpty()){
             throw new UserException("The following walmart taxonomies aren't in walmart structure: ", expList);
