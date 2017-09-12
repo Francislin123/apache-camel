616af86aff8db863654420ad4771d3bda7b866d3
Francislin dos Reis <vn0gshm@walmart.com>
2017-09-12 10:05:23 -0300

    feat(FDS-1020): FDS-1361 - Code Update and implementation of feign to display the correct message Date 12/09/2017
    (cherry picked from commit f7ab67e)

--- src/main/java/com/walmart/feeds/api/core/service/feed/ProductCollectionServiceImpl.java
+++ src/main/java/com/walmart/feeds/api/core/service/feed/ProductCollectionServiceImpl.java
@@ -3,6 +3,8 @@ package com.walmart.feeds.api.core.service.feed;
 import com.walmart.feeds.api.client.tagadmin.TagAdmimCollectionClient;
 import com.walmart.feeds.api.client.tagadmin.TagAdminCollection;
 import com.walmart.feeds.api.core.exceptions.UserException;
+import feign.Response;
+import feign.codec.ErrorDecoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Service;
  * Created by vn0gshm on 31/08/17.
  */
 @Service
-public class ProductCollectionServiceImpl implements ProductCollectionService {
+public class ProductCollectionServiceImpl implements ProductCollectionService, ErrorDecoder {
 
     @Autowired
     private TagAdmimCollectionClient tagAdminCollectionClient;
+
+    private final ErrorDecoder defaultErrorDecoder = new Default();
 
     @Override
     public void validateCollectionExists(Long collectionId) {
@@ -23,5 +27,14 @@ public class ProductCollectionServiceImpl implements ProductCollectionService {
         if (tagAdminCollection == null || !"ACTIVE".equals(tagAdminCollection.getStatus())) {
             throw new UserException(String.format("TagAdmin collection '%d' not found or not active!", collectionId));
         }
+    }
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+
+        if (response.status() >= 400 && response.status() <= 599) {
+            return new UserException(String.format("TagAdmin collection not found!"));
+        }
+        return defaultErrorDecoder.decode(methodKey, response);
     }
 }
