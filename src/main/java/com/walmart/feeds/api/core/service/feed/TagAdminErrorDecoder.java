7476edd938c5295fa256d6eccea176b59ba2a785
Leandro Costa <lcosta5@walmart.com>
2017-09-14 11:42:04 -0300

    [fds-1020] Bug fix don't accepted collectionId null, but this field is not required.

--- src/main/java/com/walmart/feeds/api/core/service/feed/TagAdminErrorDecoder.java
+++ src/main/java/com/walmart/feeds/api/core/service/feed/TagAdminErrorDecoder.java
@@ -5,17 +5,11 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 
 public class TagAdminErrorDecoder implements ErrorDecoder {
-    final ErrorDecoder defaultErrorDecoder = new Default();
 
     public TagAdminErrorDecoder() {
     }
 
     @Override
     public Exception decode(String methodKey, Response response) {
-
-        if (response.status() >= 400 && response.status() <= 599) {
-            return new UserException(String.format("TagAdmin collection not found!"));
-        }
-        return defaultErrorDecoder.decode(methodKey, response);
-    }
-}
+        return new UserException("TagAdmin collection not found!");
+    }}
