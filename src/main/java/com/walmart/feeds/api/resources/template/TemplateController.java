be21709672d1c65eef303c713658d24e3d510ab1
Adisson Gomes <vn03fh2@walmart.com>
2017-09-21 11:20:18 -0300

    feat(FDS-1579): merge request discussions

--- src/main/java/com/walmart/feeds/api/resources/template/TemplateController.java
+++ src/main/java/com/walmart/feeds/api/resources/template/TemplateController.java
@@ -3,7 +3,11 @@ package com.walmart.feeds.api.resources.template;
 import com.walmart.feeds.api.core.repository.template.model.TemplateEntity;
 import com.walmart.feeds.api.core.service.template.TemplateService;
 import com.walmart.feeds.api.resources.template.response.TemplateResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +22,13 @@ public class TemplateController {
     @Autowired
     private TemplateService templateService;
 
-    @RequestMapping("{slug}")
+    @ApiOperation(value = "Fetch a template slug",
+            httpMethod = "GET",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful fetch"),
+            @ApiResponse(code = 404, message = "Template not found")})
+    @RequestMapping(value = "{slug}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<TemplateResponse> fetchBySlug(@PathVariable("slug") String slug) {
         TemplateEntity templateEntity = templateService.fetchBySlug(slug);
         TemplateResponse response = TemplateResponse.builder()
