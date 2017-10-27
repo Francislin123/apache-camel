fe9804bc5569bceec2fa5f8fa20e4eabaf13db3f
Adisson Gomes <vn03fh2@walmart.com>
2017-10-27 19:28:33 -0200

    refactor(db): include new feed for test on db.feeds-test.sql

--- src/main/resources/db/changelog/db.feeds-test.sql
+++ src/main/resources/db/changelog/db.feeds-test.sql
@@ -43,6 +43,8 @@ Insert into FEEDS_ADMIN.TAXONOMY_MAPPING (ID,PARTNER_TAXONOMY_ID, PARTNER_PATH_I
 SYS_GUID(), (SELECT id FROM PARTNER_TAXONOMY WHERE slug = 'zoom'), '123','Armas', 'Armas > Rifles', CURRENT_TIMESTAMP,'teste');
 Insert into FEEDS_ADMIN.TAXONOMY_MAPPING (ID,PARTNER_TAXONOMY_ID, PARTNER_PATH_ID, WALMART_PATH,PARTNER_PATH,CREATION_DATE, USER_LOGIN) values (
 SYS_GUID(), (SELECT id FROM PARTNER_TAXONOMY WHERE slug = 'zoom'), '123','Computadores > NoteBooks','PC > Notebooks', CURRENT_TIMESTAMP,'teste');
+Insert into FEEDS_ADMIN.TAXONOMY_MAPPING (ID,PARTNER_TAXONOMY_ID, PARTNER_PATH_ID, WALMART_PATH,PARTNER_PATH,CREATION_DATE, USER_LOGIN) values (
+SYS_GUID(), (SELECT id FROM PARTNER_TAXONOMY WHERE slug = 'zoom'), '123','Games > Playstation 3 > Jogos para PS3', 'Consoles > Playstation > Jogos PS3', CURRENT_TIMESTAMP,'teste');
 
 Insert into FEEDS_ADMIN.TAXONOMY_BLACKLIST (ID,NAME,SLUG,CREATION_DATE,UPDATE_DATE,USER_LOGIN) values (
 SYS_GUID(),'armas','armas', CURRENT_TIMESTAMP, null,'123e80c6-ee55-48ff-9d1a-f6a7cf902465');
@@ -107,3 +109,20 @@ Insert into FEEDS_ADMIN.FEED_UTMS (FEED_ID,UTM_TYPE,UTM_VALUE) values ((SELECT i
 Insert into FEEDS_ADMIN.FEED_UTMS (FEED_ID,UTM_TYPE,UTM_VALUE) values ((SELECT id FROM FEED WHERE slug = 'full'), 'utm_campaign','feed-campaign');
 
 Insert into FEEDS_ADMIN.TERMS_FEEDS (TERMS_BLACKLIST_ID,FEED_ID) values ((SELECT id FROM TERMS_BLACKLIST WHERE slug = 'armas'), (SELECT id FROM FEED WHERE slug = 'full'));
+
+-- GOOGLE
+
+INSERT INTO FEED(id, name, slug, PARTNER_TAXONOMY_ID, partner_id, template_id, "TYPE", notification_method, notification_format, flag_active, creation_date, user_login) VALUES (
+sys_guid(),
+'google',
+'google',
+(SELECT id FROM PARTNER_TAXONOMY WHERE slug='zoom'),
+(SELECT id FROM PARTNER WHERE slug='zoom'),
+(SELECT id FROM FEED_TEMPLATE WHERE slug='google-xml-template'),
+'FULL', 'FILE', 'XML',
+1, CURRENT_TIMESTAMP, 'user'
+);
+
+Insert into FEEDS_ADMIN.FEED_UTMS (FEED_ID,UTM_TYPE,UTM_VALUE) values ((SELECT id FROM FEED WHERE slug = 'google'), 'utm_source','google-pla');
+Insert into FEEDS_ADMIN.FEED_UTMS (FEED_ID,UTM_TYPE,UTM_VALUE) values ((SELECT id FROM FEED WHERE slug = 'google'), 'utm_medium','feed-medium');
+Insert into FEEDS_ADMIN.FEED_UTMS (FEED_ID,UTM_TYPE,UTM_VALUE) values ((SELECT id FROM FEED WHERE slug = 'google'), 'utm_campaign','feed-campaign');
